8b7ef7f80725b51b24ef1c73272a51bcd02a0df1
nassimb666 <bnassim750@gmail.com>
2023-12-22 12:24:26 +0100

    ajout de la liste en php

--- index.php
+++ index.php
@@ -31,13 +31,58 @@
     <h3>Liste des nouveaux diplômés :</h3>
 
     <div class="liste">
+    <?php
+    include('./assets/data/data.php');
+
+    $images = [
+        "./assets/img/portrait-1.jpg",
+        "./assets/img/portrait-2.jpg",
+        "./assets/img/portrait-3.jpg",
+        "./assets/img/portrait-4.jpg",
+        "./assets/img/portrait-5.jpg",
+        "./assets/img/portrait-6.jpg",
+        "./assets/img/portrait-7.jpg",
+        "./assets/img/portrait-8.jpg",
+        "./assets/img/portrait-9.jpg",
+        "./assets/img/portrait-10.jpg",
+        "./assets/img/portrait-11.jpg",
+        "./assets/img/portrait-12.jpg",
+        "./assets/img/portrait-13.jpg",
+        "./assets/img/portrait-14.jpg",
+        "./assets/img/portrait-15.jpg",
+        "./assets/img/portrait-16.jpg",
+        "./assets/img/portrait-17.jpg",
+        "./assets/img/portrait-18.jpg",
+        "./assets/img/portrait-19.jpg",
+        "./assets/img/portrait-20.jpg",
+    ];
+
+    $imageIndex = 0;
     
+    foreach ($data as $person) {
+        echo '<div class="card">';
+        echo '<img src="' . $images[$imageIndex] . '" alt="" class="portrait">'.'<br>';
+        echo '<strong>Nom:</strong> ' . $person['name'] . '<br>';
+        echo '<strong>Téléphone:</strong> ' . $person['phone'] . '<br>';
+        echo '<strong>Email:</strong> ' . $person['email'] . '<br>';
+        echo '<strong>Région:</strong> ' . $person['region'] . '<br>';
+        echo '<button>Contactez</button>';
+        echo '</div>';
+
+        
+        $imageIndex = ($imageIndex + 1) % count($images);
+    }
+?>
+
+
+
     </div>
+    
 
     <div class="parallax"></div>
 
     <?php include './components/footer.php'; ?>
 
-    <script src="./assets/script/script.js"></script>
+    <!-- <script src="./assets/script/script.js"></script> -->
 </body>
 </html>
